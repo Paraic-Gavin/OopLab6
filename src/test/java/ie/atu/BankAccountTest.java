db8895d37592edb2e506e3e326c7a079dea2d79f
ParaicGavin-STUDENT <g00465477@atu.ie>
2025-11-19 17:38:31 +0000

    Lab 6: Bank Account

--- src/test/java/ie/atu/BankAccountTest.java
+++ src/test/java/ie/atu/BankAccountTest.java
@@ -1,36 +1,39 @@
 package ie.atu;
 
+import ie.atu.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 public class BankAccountTest {
 
     BankAccount account = new BankAccount();
 
     @BeforeEach
-    void setUp()
+    void setup()
     {
         account = new BankAccount();
     }
 
+
+    @Test
     void constructorInitialisation()
     {
-        account = new BankAccount("ACC12345","Paraic",100);
+        account = new BankAccount("ACC12345", "Paraic", 100);
         assertEquals("ACC12345", account.getAccNo());
-        assertEquals("Paraic", account.getAccNo());
-        assertEquals(100, account.getAccNo());
-
-
+        assertEquals("Paraic", account.getName());
+        assertEquals(100, account.getBalance());
 
 
     }
     @Test
-    void constructorNegativeInitialisation() {
-        Exception ex = assertThrows(IllegalArgumentException.class, () -> new BankAccount("ACC12345","Paraic",-100));
+    void constructorNegativeInitialisation()
+    {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new BankAccount("ACC12345", "Paraic", -100));
         assertEquals("Balance must be greater than 0.", ex.getMessage());
     }
+
+
 }
